833d664995e8a4d88886a2e8cb54b8ab18bda192
YASHU TANDON <yashutandon56@gmail.com>
2025-10-19 02:31:39 +0530

    add trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import prisma from "@/lib/db";
-import Image from "next/image";
+"use client";
 
-export default async function Home() {
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
-  const user = await prisma.user.findMany();
+export default  function Home() {
+
+  const trpc=useTRPC();
+  const {data:user}=useQuery(trpc.getUsers.queryOptions());
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
      {JSON.stringify(user)}
